8fcbd9bf30fdd4b009343a01e5b6379e483c734e
felipeportari <felipe.portari7@gmail.com>
2025-11-27 15:17:06 -0300

    Criando novos cards

--- config.php
+++ config.php
@@ -28,18 +28,24 @@ $cards =  [
         'title' => 'Landing Starbucks',
         'text'  => 'Landing page criada utilizando o Starbucks como inspiração, focada em design moderno e experiência visual atraente.',
         'link'   => 'https://github.com/felipeportari/starbucks_landing',
-        'image' => 'src/homem-de-tiro-medio-usando-oculos-vr_23-2149126949.webp'
+        'image' => 'src/starbucks-1262392_640.webp'
     ],
     [
         'title' => 'Projeto Globo Terrestre',
         'text'  => 'Projeto em Three.js apresentando um globo terrestre interativo, desenvolvido para aprender e explorar conceitos de 3D na web.',
         'link'   => 'https://github.com/felipeportari/three.js-project',
-        'image' => 'src/homem-de-tiro-medio-usando-oculos-vr_23-2149126949.webp'
+        'image' => 'src/globe.webp'
     ],
     [
         'title' => 'Estoque Online',
         'text'  => 'Sistema de estoque online criado para facilitar checagens e marcações sem o uso de papel, tornando o processo mais ágil e organizado.',
         'link'   => 'https://github.com/felipeportari/stock_checker',
-        'image' => 'src/homem-de-tiro-medio-usando-oculos-vr_23-2149126949.webp'
+        'image' => 'src/gestaoclick-estoque-shoope-mercado-livre.webp'
+    ],
+    [
+        'title' => 'Passion Clock',
+        'text'  => 'Relógio online criado de brincadeira utilizando HTML, CSS e JavaScript, inspirado no meme “graphic design is my passion”.',
+        'link'   => 'https://github.com/felipeportari/passion-clock',
+        'image' => 'src/original-56b19720d83b146e748d69554e264aff.webp'
     ]
 ];
--- index.php
+++ index.php
@@ -4,10 +4,15 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+    rel="stylesheet">
+
   <title>PHP</title>
+
   <style>
     * {
       margin: 0;
@@ -20,6 +25,7 @@
       background-color: #010510;
       color: #f5f5f5;
       gap: 20px;
+      overflow-x: hidden;
     }
 
     h1.title {
@@ -56,10 +62,12 @@
       width: 50%;
     }
 
-    .container img {
+    img.about {
       width: 100%;
-      height: auto;
-      border-radius: 40px;
+      max-width: 500px;
+      aspect-ratio: 1/1;
+      object-fit: cover;
+      border-radius: 50%;
     }
 
     @media (max-width: 1024px) {
@@ -107,7 +115,6 @@
       width: fit-content;
     }
 
-
     @media (max-width: 1024px) {
       div.technologies {
         justify-content: center;
@@ -129,30 +136,184 @@
       }
     }
 
+    .card-container {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+      row-gap: 30px;
+    }
+
+    .cards {
+      display: flex;
+      flex-direction: row;
+      max-width: 49%;
+      overflow: hidden;
+      border-radius: 25px;
+      position: relative;
+    }
+
+    .cards div {
+      max-width: 48%;
+    }
+
+    img.card-image {
+      max-width: 100%;
+      object-fit: cover;
+      height: 100%;
+      object-position: top;
+      display: block;
+    }
+
+    .info {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      padding: 20px;
+    }
+
+    .card-button {
+      background-color: transparent;
+      color: white;
+      padding: 12px 20px;
+      border: 2px solid #4A90E2;
+      border-radius: 6px;
+      font-size: 16px;
+      font-family: Arial, sans-serif;
+      cursor: pointer;
+      transition: 0.3s;
+    }
+
+    .card-button:hover {
+      background-color: #4A90E2;
+      scale: 1.05;
+    }
+
+    .cards {
+      display: flex;
+      flex-direction: row;
+      max-width: 49%;
+      overflow: hidden;
+      border-radius: 25px;
+      position: relative;
+    }
+
+    .cards .imagem {
+      width: 48%;
+      overflow: hidden;
+      position: relative;
+    }
+
+    .cards .imagem img.card-image {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      display: block;
+    }
+
+    .cards .info {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      padding: 20px;
+      width: 52%;
+    }
+
+    @media (max-width: 1024px) {
+      .cards {
+        flex-direction: column;
+        align-items: center;
+        max-width: 100%;
+        border-radius: 20px;
+      }
+
+      .cards .imagem {
+        width: 100%;
+        height: auto;
+      }
+
+      .cards .imagem img.card-image {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        border-top-left-radius: 20px;
+        border-top-right-radius: 20px;
+      }
+
+      .cards .info {
+        width: 100%;
+        padding: 20px;
+      }
+
+      .cards .info p {
+        margin-bottom: 10px;
+      }
+    }
+
+    @media (max-width: 768px) {
+      .cards {
+        flex-direction: column;
+      }
+
+      .cards .imagem {
+        width: 100%;
+        max-height: 500px;
+      }
+    }
   </style>
 </head>
 
-<body> <?php include 'config.php' ?> <section style="min-height: 80vh">
+<body>
+
+  <?php include 'config.php' ?>
+
+  <section style="min-height: 80vh">
     <div class="container">
+
       <div style="display: flex; flex-direction: column; gap: 15px">
-        <h1 class="title">Olá! Sou Felipe Portari. Transformo <span>ideias</span> em <span>sistemas</span></h1>
+        <h1 class="title">
+          Olá! Sou Felipe Portari. Transformo <span>ideias</span> em <span>sistemas</span>
+        </h1>
+
         <div class="technologies">
           <?php foreach ($technologies as $technology): ?>
             <div class="technology"
-              style="background-color:<?= $technology['color'] ?>50;
-            border:1.5px solid <?= $technology['color'] ?>70">
+              style="background-color: <?= $technology['color'] ?>50;
+                        border: 1.5px solid <?= $technology['color'] ?>70;">
               <?= $technology['name'] ?>
             </div>
           <?php endforeach; ?>
+
+          <p>
+            Sou desenvolvedor backend com experiência em PHP, Laravel, WordPress e Filament, criando sistemas,
+            sites e painéis administrativos eficientes e bem estruturados. Trabalho com integração de APIs,
+            Livewire, bancos de dados MySQL e automações, além de configurar hospedagens, e-mails e infraestrutura
+            para garantir que tudo funcione de forma estável. Tenho facilidade em aprender novas tecnologias,
+            resolver problemas e transformar necessidades reais em soluções práticas e escaláveis.
+          </p>
         </div>
       </div>
-      <div> <img src="src/man-working-night.webp" alt="" style="border-radius: 40px"> </div>
+
+      <div style="display: flex; justify-content: center">
+        <img class="about" src="src/man-working-night.webp" alt="">
+      </div>
+
     </div>
   </section>
-  <section class="neon-section">
-    <div class="card-grid">
-      <?php foreach ($cards as $card): ?>
 
+  <section>
+    <div class="container card-container">
+      <?php foreach ($cards as $card): ?>
+        <div class="cards">
+          <div class="imagem">
+            <img class="card-image" src="<?= $card['image'] ?>" alt="">
+          </div>
+          <div class="info">
+            <p> <?= $card['text'] ?></p>
+            <a href="<?= $card['link'] ?>">
+              <button class="card-button">Ver Mais</button>
+            </a>
+          </div>
+        </div>
       <?php endforeach; ?>
     </div>
   </section>
